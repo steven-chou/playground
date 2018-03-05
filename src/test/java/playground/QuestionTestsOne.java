0fddba7b7c70d8d1de8a3bf1fe74ef41100a5050
steven-chou <choucw1206@gmail.com>
2018-03-05 00:15:04 -0800

    New test

--- src/test/java/playground/QuestionTestsOne.java
+++ src/test/java/playground/QuestionTestsOne.java
@@ -1,6 +1,7 @@
 package playground;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
 
 import java.util.*;
 
@@ -14,10 +15,10 @@ public class QuestionTestsOne {
      */
     @Test
     void powerSet() {
-        powerSet(List.of("A", "B", "C"));
+        System.out.println(powerSet(List.of("A", "B", "C")));
     }
 
-    static <T> List<List<T>> powerSet(List<T> input) {
+    private static <T> List<List<T>> powerSet(List<T> input) {
         List<List<T>> results = new ArrayList<>();
         for (T element : input) {
 
@@ -41,4 +42,39 @@ public class QuestionTestsOne {
         return results;
     }
 
+    /**
+     * Write an algorithm that prints all numbers between 1 and n, replacing multiples
+     * of 3 with the String Fizz, multiples of 5 with Buzz, and multiples of 15 with
+     * FizzBuzz.
+     */
+    @Test
+    void fizzBuzz() {
+        System.out.println(solveFizzBuzz(5));
+    }
+
+    private static List<String> solveFizzBuzz(final int n) {
+        final List<String> result = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++) {
+            final String word = toWord(3, i, "Fizz") + toWord(5, i, "Buzz");
+            if (StringUtils.isEmpty(word)) {
+                result.add(Integer.toString(i));
+            } else {
+                result.add(word);
+            }
+        }
+        return result;
+    }
+
+    private static String toWord(final int divisor, final int value, final String word) {
+        return value % divisor == 0 ? word : "";
+    }
+
+    /**
+     * Returns a Fibonacci sequence from 1 to n.
+     *
+     */
+    @Test
+    void fibonacciSequence() {
+
+    }
 }
